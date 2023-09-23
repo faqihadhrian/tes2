8267e168385f08f22e2d15aa955ed42a5a0b7769
faqihadhrian <faqih.adh@gmail.com>
2023-09-23 02:21:54 -0700

    tes2

---
+++ perkenalan/Perkenalan.java
@@ -0,0 +1,10 @@
+package perkenalan;
+public class Perkenalan{
+public static void main(String[] args) {
+
+    System.out.println("perkenalkan nama saya ");
+    System.out.println("Muhammad Faqih Adhrian Ramadhan");
+    System.out.println("Asal Batulicin");
+    System.out.println("hobinya suka kamu");
+}
+}
